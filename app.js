cabfd2c4984754749374b436cd8c8e019717cd69
NemoArtifex <mweakes@gmail.com>
2026-02-22 18:33:08 -0800

    further update to app.js, updated url in fetch tables

--- app.js
+++ app.js
@@ -235,7 +235,8 @@ async function fetchTableData(tableName) {
         });
 
         // FIXED URL STRING: Ensuring no stray characters or brackets exist
-        const url = `https://graph.microsoft.com{fileName}:/workbook/tables/${tableName}/rows`;
+        const url = `https://graph.microsoft.com/v1.0/me/drive/root:/${fileName}:/workbook/tables/${tableName}/rows`;
+
 
         const response = await fetch(url, {
             headers: { 'Authorization': `Bearer ${tokenResponse.accessToken}` }
